a1e90cf7e907a51ce2356635d258bdebe46e097b
iakubi <enderendy121@gmail.com>
2023-10-11 00:18:55 -0400

    Starting out

---
+++ Resource.java
@@ -0,0 +1,40 @@
+public class Resource {
+    private String name;
+    private int quantity;
+    private boolean isCritical;
+
+    public Resource(String name){
+        this.name = name;
+        this.quantity = 0;
+        this.isCritical = false;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+
+    public boolean isCritical(){
+        return isCritical;
+    }
+
+    public void setIsCrticial(boolean isCritical){
+        this.isCritical = isCritical;
+    }
+
+    public void add(int amount) {
+        quantity += amount;
+    }
+
+    public void consume(int amount) {
+        if (quantity >= amount) {
+            quantity -= amount;
+        } else {
+            quantity = 0;
+            System.out.println("Not enough " + name + " to consume.");
+        }
+    }
+}
---
+++ TextManagementGame.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class TextManagementGame{
+    private int round; 
+    private ArrayList<Resource> resources = new ArrayList<Resource>();
+    
+}
